23aacfa7da83fbd0400989e434dd09eb10733334
chailong <chailongyes@qq.com>
2020-06-17 15:05:00 +0800

    更改公网地址

--- src/main.js
+++ src/main.js
@@ -9,7 +9,7 @@ import axios from 'axios';
 import './assets/fonts/iconfont.css'
 
 
-axios.defaults.baseUrl='http://127.0.0.1:8888/api/private/v1/';
+axios.defaults.baseUrl='http://1.207.114.131:8888/api/private/v1/';
 //给全局添加一个原型方法
 Vue.config.productionTip = false;
 Vue.use(element);
